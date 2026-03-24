eb429530cfb95d45a2752a75b19c00b6d58906eb
nicholasfocke <nicholasfocke05@gmail.com>
2026-03-24 10:13:09 -0300

    Last test git

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,7 +3,7 @@ package application;
 public class Program {
 
 	public static void main(String[] args) {
-		System.out.println("Teste novo ");
+		System.out.println("Teste novo ultima vez");
 
 	}
 
